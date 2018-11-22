80b58abba7c66b512d58f567973b1f497d204bbe
Erick León <erick.leon21@gmail.com>
2018-11-21 23:16:07 -0600

    first commit

---
+++ Views/modules/usuarios/salir.php
@@ -0,0 +1,9 @@
+<?php session_destroy(); ?>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">Haz Salido de la Aplicacion.!</h1>
+    <p class="lead">Gracias Por Su Preferencia</p>
+  </div>
+</div>
+
+<meta http-equiv="refresh" content="3; url=index.php"/>
